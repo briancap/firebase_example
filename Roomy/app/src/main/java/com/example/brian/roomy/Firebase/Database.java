dc9255c1a3d2874ade2fa43716f6a8a71ae67653
briancap <btc5081@gmail.com>
2017-07-12 23:16:20 -0400

    add Firebase dependencies and Class

---
+++ Roomy/app/src/main/java/com/example/brian/roomy/Firebase/Database.java
@@ -0,0 +1,12 @@
+package com.example.brian.roomy.Firebase;
+
+/**
+ * This class deals with all methods relating to the Firebase Real Time Database
+ */
+
+public class Database {
+
+    public Database(){
+
+    }
+}
