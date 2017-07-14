6e15a5ec111050533059b54a956f93b0f104b554
briancap <btc5081@gmail.com>
2017-07-13 22:22:47 -0400

    add options menu resource folder, xml file, and necessary methods to MainActivity

--- Roomy/app/src/main/java/com/example/brian/roomy/MainActivity.java
+++ Roomy/app/src/main/java/com/example/brian/roomy/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.brian.roomy;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,6 +14,18 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.menu_main_activity, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        int id = item.getItemId();
+
+        return super.onOptionsItemSelected(item);
+    }
 
 
 
