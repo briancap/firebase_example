d9ba0d88413cdbcd9f8037fe98efe21ff410165c
briancap <btc5081@gmail.com>
2017-07-15 15:48:57 -0400

    Add field and corresponding getters and setters to Tenant class

--- Roomy/app/src/main/java/com/example/brian/roomy/Firebase/Tenant.java
+++ Roomy/app/src/main/java/com/example/brian/roomy/Firebase/Tenant.java
@@ -1,12 +1,86 @@
 package com.example.brian.roomy.Firebase;
 
 /**
- * This class deals with all methods relating to the Firebase Real Time Database
+ * This class deals with all methods relating to the Tenant Node of the Firebase DB
  */
 
 public class Tenant {
+    private String tenantId;
+    private String name;
+    private String phoneNumber;
+    private String leaseId;
+    private String addressId;
+    private String realtyCompanyId;
+    private String paymentId;   //venom or other
+    private String authId;      //google, phone number, fingerprint
 
+//CONSTRUCTORS
     public Tenant(){
+    }
 
+
+//GETTERS AND SETTERS
+    public String getTenantId() {
+        return tenantId;
+    }
+
+    public void setTenantId(String tenantId) {
+        this.tenantId = tenantId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getLeaseId() {
+        return leaseId;
+    }
+
+    public void setLeaseId(String leaseId) {
+        this.leaseId = leaseId;
+    }
+
+    public String getAddressId() {
+        return addressId;
+    }
+
+    public void setAddressId(String addressId) {
+        this.addressId = addressId;
+    }
+
+    public String getRealtyCompanyId() {
+        return realtyCompanyId;
+    }
+
+    public void setRealtyCompanyId(String realtyCompanyId) {
+        this.realtyCompanyId = realtyCompanyId;
+    }
+
+    public String getPaymentId() {
+        return paymentId;
+    }
+
+    public void setPaymentId(String paymentId) {
+        this.paymentId = paymentId;
+    }
+
+    public String getAuthId() {
+        return authId;
+    }
+
+    public void setAuthId(String authId) {
+        this.authId = authId;
     }
 }
